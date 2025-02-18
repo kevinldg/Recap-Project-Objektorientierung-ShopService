71bacb1bd625053d2091a929de6a47e71537ee3c
kevinldg <kevinloeding@outlook.de>
2025-02-18 12:09:49 +0100

    Add timestamp to orders

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -1,10 +1,12 @@
 import lombok.With;
 
+import java.time.Instant;
 import java.util.List;
 
 public record Order(
         String id,
         List<Product> products,
-        @With OrderStatus status
+        @With OrderStatus status,
+        Instant timestamp
 ) {}
 
