fe106b89b12a3dfdd26246a726f86168dcc09848
kevinldg <kevinloeding@outlook.de>
2025-02-18 11:59:54 +0100

    Throw exception in ShopService when productId is not found

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -10,11 +10,8 @@ public class ShopService {
     public Order addOrder(List<String> productIds) {
         List<Product> products = new ArrayList<>();
         for (String productId : productIds) {
-            Product productToOrder = productRepo.getProductById(productId);
-            if (productToOrder == null) {
-                System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
-                return null;
-            }
+            Product productToOrder = productRepo.getProductById(productId)
+                    .orElseThrow(() -> new IllegalArgumentException("Product mit der Id: " + productId + " existiert nicht!"));
             products.add(productToOrder);
         }
 
